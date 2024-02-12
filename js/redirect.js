e88b1d35416efdfb73e801c70a4a1ff04014b2d6
G4br1elF <bielfernandes83@gmail.com>
2024-02-12 12:25:25 -0300

    commit

---
+++ js/redirect.js
@@ -0,0 +1,8 @@
+const Saiba = document.querySelector(".saiba");
+const Git = document.querySelector(".github");
+Saiba.addEventListener("click", () => {
+  window.location.href = "sobre.html";
+});
+Git.addEventListener("click", () => {
+  window.location.href = "https://github.com/G4br1elF/";
+});
